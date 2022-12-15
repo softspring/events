bd4341c0a1a4cc96f8819893c9db7f4cd281b035
Javi H. Gil <javihgil@softspring.es>
2022-12-15 15:58:44 +0100

    Add phpstan

--- src/ViewEvent.php
+++ src/ViewEvent.php
@@ -2,31 +2,30 @@
 
 namespace Softspring\Component\Events;
 
+use ArrayObject;
 use Symfony\Component\HttpFoundation\Request;
 
 class ViewEvent extends RequestEvent
 {
-    protected \ArrayObject $data;
+    protected ArrayObject $data;
 
     /**
-     * @param \ArrayObject|array $data
+     * @param ArrayObject|array $data
      */
     public function __construct($data, ?Request $request = null)
     {
         parent::__construct($request);
 
-        if (!$data instanceof \ArrayObject && !is_array($data)) {
+        if (!is_array($data)) {
             throw new \InvalidArgumentException('$data must be an ArrayObject or just an array');
         }
 
-        if (is_array($data)) {
-            $data = new \ArrayObject($data);
-        }
+        $data = new \ArrayObject($data);
 
         $this->data = $data;
     }
 
-    public function getData(): \ArrayObject
+    public function getData(): ArrayObject
     {
         return $this->data;
     }
